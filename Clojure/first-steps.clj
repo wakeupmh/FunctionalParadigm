84f85b9304016c3a92928c72bcb7fe8faedae6af
wakeupmh <wakeupmh@gmail.com>
2019-10-05 19:40:34 -0300

    using let (define scope in clj)

--- Clojure/first-steps.clj
+++ Clojure/first-steps.clj
@@ -1,7 +1,8 @@
 (defn value-discounted 
     "Function to apply 15% discount in a value"
     [value]
-    (* value (- 1 0.15)))
+    (let [discount 0.15]
+        (* value (- 1 discount))))
 
 (value-discounted 1000)
 

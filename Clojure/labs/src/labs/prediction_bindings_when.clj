cf28debd3a8c12681a8d2abe6f8d5fdb59a38669
wakeupmh <wakeupmh@gmail.com>
2019-10-12 12:53:35 -0300

    predictions-binding-when

--- Clojure/labs/src/labs/prediction_bindings_when.clj
+++ Clojure/labs/src/labs/prediction_bindings_when.clj
@@ -1,10 +1,19 @@
 (ns labs.prediction-bindings-when)
+;Predicate
+(defn apply-discount?
+  [value]
+  (> value 100))
 
 (defn discounted-value
   [value]
-  (if (> value 100)
+  (if (apply-discount? value)
     (let [discount-tax (/ 15 100)
           discount (* value discount-tax)]
       (- value discount))
     value))
 
+(println (apply-discount? 1000))
+(println (discounted-value 1000))
+
+;Binding is executed in run time
+;When statement is used for a if without else

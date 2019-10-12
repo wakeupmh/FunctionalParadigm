571501b276330b55d060a8c4cbb8238441445373
wakeupmh <wakeupmh@gmail.com>
2019-10-12 12:58:58 -0300

    function as param

--- Clojure/labs/src/labs/prediction_bindings_when.clj
+++ Clojure/labs/src/labs/prediction_bindings_when.clj
@@ -1,19 +1,18 @@
 (ns labs.prediction-bindings-when)
 ;Predicate
-(defn apply-discount?
+(defn expensive-than-100?
   [value]
   (> value 100))
 
 (defn discounted-value
-  [value]
-  (if (apply-discount? value)
+  [expensive-than-100? value]
+  (if (expensive-than-100? value)
     (let [discount-tax (/ 15 100)
           discount (* value discount-tax)]
       (- value discount))
     value))
 
-(println (apply-discount? 1000))
-(println (discounted-value 1000))
+(println (discounted-value expensive-than-100? 1000))
 
-;Binding is executed in run time
+;Binding (function calls function) is executed in run time
 ;When statement is used for a if without else

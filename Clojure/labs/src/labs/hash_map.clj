1cb256449c30db49d61e2fa43a5732d480b9d784
wakeupmh <wakeupmh@gmail.com>
2019-10-21 23:54:31 -0300

    handling nested maps

--- Clojure/labs/src/labs/hash_map.clj
+++ Clojure/labs/src/labs/hash_map.clj
@@ -20,3 +20,30 @@
 (println (update store :backpack #(- % 2)))
 
 (println (dissoc store :t-shirt))
+
+;nested
+
+(def demand {:backpack {:quantity 4 :price 80}
+             :t-shirt {:quantity 5 :price 40}})
+
+(assoc demand :keychain {:quantity 3 :price 15})
+
+(println (demand :backpack))                                ;map as a function (if doesnt exists demand throw exception)
+(println (get demand :backpack))                            ;get method
+(println (get demand :chair))                               ;nil
+(println (get demand :chair {}))                            ;default value {}
+(println (:chair demand {}))                                ;as a get or map as fuction with default value
+(println (:chair demand))
+
+; THREADING
+(println (:quantity (:backpack demand)))
+(println (update-in demand [:backpack :quantity] inc))      ;update nested maps
+
+(println (-> demand
+             :backpack 
+             :quantity))
+
+
+
+
+

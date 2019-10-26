709383f4ff6fdc89bb3048770899de0d8b19abdf
wakeupmh <wakeupmh@gmail.com>
2019-10-26 16:23:43 -0300

    group by in clojure and require

--- Clojure/shop/src/shop/map_schema.clj
+++ Clojure/shop/src/shop/map_schema.clj
@@ -4,3 +4,23 @@
 (println (l.db/all-demands))
 
 (println (group-by :user (l.db/all-demands)))
+
+(println (map count (vals (group-by :user (l.db/all-demands)))))
+
+(->> (l.db/all-demands)
+     (group-by :user)
+     vals
+     (map count)
+     println)
+
+(defn total-per-user [[user demands]]
+  {
+    :user (count demands)
+    :total ()
+   }
+  )
+
+(->> (l.db/all-demands)
+     (group-by :user)
+     (map total-per-user)
+     println)

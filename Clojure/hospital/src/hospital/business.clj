7c16b21a005e243f6adec3e17146346c01b37cea
wakeupmh <wakeupmh@gmail.com>
2019-10-30 22:17:59 -0300

    handling queue in thread first

--- Clojure/hospital/src/hospital/business.clj
+++ Clojure/hospital/src/hospital/business.clj
@@ -1,10 +1,27 @@
-
 (ns hospital.business)
+
+(defn full?
+  [hospital sector]
+  (-> hospital
+      (get sector)
+      count
+      (< 5))
+
+
+  queue (get hospital sector)
+  current-length (count queue)
+  fit? (< current-lenght 5))
 
 (def arrive-at
   [hospital sector person]
-  (update hospital sector conj person))
+    (if (full? hospital sector )
+      (throw (ex-info "Queue is already full" {:try-add person}))
+      (update hospital sector conj person)))
 
 (def call
   [hospital sector]
   (update hospital sector pop))
+
+
+
+

629a29e2b5ef44dbdf72910b3d80add0edf69d85
wakeupmh <wakeupmh@gmail.com>
2019-11-02 10:58:04 -0300

    using atoms

--- Clojure/hospital/src/hospital/workday2.clj
+++ Clojure/hospital/src/hospital/workday2.clj
@@ -8,5 +8,14 @@
 
 (defn big-atom []
   (let [hospital-lima (atom {:wait h.model/empty-queue})]
+    (pprint hospital-lima)
+    (pprint (deref hospital-lima));extract a value in atom
+    (pprint @hospital-lima);extract a value in atom
+
+    (pprint (assoc @hospital-lima :exams h.model/empty-queue)) ;doesn't change the value, jus add a value into a clone
+    ;!-> refers to side effect
+    (swap! hospital-lima assoc :exams h.model/empty-queue) ;Change the value of an atom, side effect.....
+    (pprint @hospital-lima)
+
 
     ))

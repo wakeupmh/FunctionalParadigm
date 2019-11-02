55582cf199646266d8788a7154253edf8c1b11ab
wakeupmh <wakeupmh@gmail.com>
2019-11-02 11:21:03 -0300

    using swap!

--- Clojure/hospital/src/hospital/workday2.clj
+++ Clojure/hospital/src/hospital/workday2.clj
@@ -17,5 +17,12 @@
     (swap! hospital-lima assoc :exams h.model/empty-queue) ;Change the value of an atom, side effect.....
     (pprint @hospital-lima)
 
+    ;update a value without side effect, using deref
+    (update @hospital-lima :exams conj "111")
+
+    ;with side effects
+    (swap! hospital-lima update :exams conj "111")
+    (pprint @hospital-lima)
+    
 
     ))

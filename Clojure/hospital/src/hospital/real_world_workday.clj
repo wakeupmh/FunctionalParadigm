e1208ab19d982ae928315af57437b50b5b5cc2d6
wakeupmh <wakeupmh@gmail.com>
2019-11-02 14:23:58 -0300

    threading with retry

--- Clojure/hospital/src/hospital/real_world_workday.clj
+++ Clojure/hospital/src/hospital/real_world_workday.clj
@@ -6,6 +6,10 @@
 ;side effect
 (defn arrive-at! [hospital person]
       (swap! hospital h.business/arrive-at :ticket-window person))
+;swap! -> works like transaction, if can't execute, retry again
+;We can set limits on these transactions by indicating to swap! that executes the entire content of the function only once with input and output results, as if it were an atom.
+; If in the middle of this process any data breaks, the program retries.
+;With that, we no longer have to worry about locking
 
 (defn simulate-day []
   (let [hospital (atom (h.model/new-hospital))]
